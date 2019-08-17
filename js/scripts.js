332fa3ba0ddb4281f49e1d65641ee5660f1e5b12
ErVuts <ervuts@gmail.com>
2019-08-17 17:47:38 +0300

    add js function

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	'use strick';
+
+
+	$('.slider').slick({
+	prevArrow: '<button class="slider__arrow slider__arrow--prev"></button>',
+	nextArrow: '<button class="slider__arrow slider__arrow--next"></button>',
+	dots: true,
+	dotsClass: 'slider__dots'
+	});
+
+});
